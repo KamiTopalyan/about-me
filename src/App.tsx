54aff622a7ade4b3c671151213b445bdba9d6cd8
KamiTopalyan <Kami.topalyan@hisarschool.k12.tr>
2023-09-17 23:17:36 +0300

    added about-me

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Wrapper from './components/Wrapper';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <div className="header">
+          <h1>About Me</h1>
+        </div>
+        <div className="container">
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            condimentum accumsan rhoncus. Sed cursus consequat ex at dapibus.
+            Nunc ac pellentesque dolor. Nulla id leo id sapien dictum aliquet.
+            Praesent semper lobortis risus id consectetur. Maecenas eget ipsum
+            ac magna congue tristique. Nam et tellus molestie, ornare lorem non,
+            aliquet libero. Suspendisse feugiat vitae ipsum ac sollicitudin.
+          </span>
+          <img src="https://placehold.co/200x200"></img>
+        </div>
+        <div className="container">
+          <img src="https://placehold.co/200x200"></img>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            condimentum accumsan rhoncus. Sed cursus consequat ex at dapibus.
+            Nunc ac pellentesque dolor. Nulla id leo id sapien dictum aliquet.
+            Praesent semper lobortis risus id consectetur. Maecenas eget ipsum
+            ac magna congue tristique. Nam et tellus molestie, ornare lorem non,
+            aliquet libero. Suspendisse feugiat vitae ipsum ac sollicitudin.
+          </span>
+        </div>
+        <div className="container">
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            condimentum accumsan rhoncus. Sed cursus consequat ex at dapibus.
+            Nunc ac pellentesque dolor. Nulla id leo id sapien dictum aliquet.
+            Praesent semper lobortis risus id consectetur. Maecenas eget ipsum
+            ac magna congue tristique. Nam et tellus molestie, ornare lorem non,
+            aliquet libero. Suspendisse feugiat vitae ipsum ac sollicitudin.
+          </span>
+          <img src="https://placehold.co/200x200"></img>
+        </div>
+      </Wrapper>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,64 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const particlesInit = async (main: any) => {
+  console.log(main);
+  await loadFull(main);
+};
+
 root.render(
   <React.StrictMode>
+    <Particles
+        className='particles'
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          background: {
+            color: "rgb(10,10,25)",
+          },
+          fpsLimit: 60,
+          particles: {
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: {
+                enable: true,
+                minimumValue: .5,
+              },
+              value: 3,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1080,
+              },
+              limit: 0,
+              value: 800,
+            },
+            opacity: {
+              animation: {
+                enable: true,
+                minimumValue: 0.5,
+                speed: 1,
+                sync: false,
+              },
+              random: {
+                enable: true,
+                minimumValue: 0,
+              },
+              value: 1,
+            },
+          },
+        }}
+      />
     <App />
   </React.StrictMode>
 );
